789ff9c7d9c1ca21ba09f24685b4b576be751e89
Parker Ovadek <povadek@bizstream.com>
2024-08-15 09:31:08 -0400

    Upgraded to beta 81

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -13,7 +13,6 @@ import { MetaTitleComponent as MetaTitleComponent_11 } from '@payloadcms/plugin-
 import { MetaImageComponent as MetaImageComponent_12 } from '@payloadcms/plugin-seo/client'
 import { MetaDescriptionComponent as MetaDescriptionComponent_13 } from '@payloadcms/plugin-seo/client'
 import { PreviewComponent as PreviewComponent_14 } from '@payloadcms/plugin-seo/client'
-import { emptyComponent as emptyComponent_15 } from '@payloadcms/ui/shared'
 import { HorizontalRuleFeatureClient as HorizontalRuleFeatureClient_16 } from '@payloadcms/richtext-lexical/client'
 import { BlocksFeatureClient as BlocksFeatureClient_17 } from '@payloadcms/richtext-lexical/client'
 import { default as default_18 } from 'src/payload/components/BeforeDashboard'
@@ -35,7 +34,6 @@ export const importMap = {
   "@payloadcms/plugin-seo/client#MetaImageComponent": MetaImageComponent_12,
   "@payloadcms/plugin-seo/client#MetaDescriptionComponent": MetaDescriptionComponent_13,
   "@payloadcms/plugin-seo/client#PreviewComponent": PreviewComponent_14,
-  "@payloadcms/ui/shared#emptyComponent": emptyComponent_15,
   "@payloadcms/richtext-lexical/client#HorizontalRuleFeatureClient": HorizontalRuleFeatureClient_16,
   "@payloadcms/richtext-lexical/client#BlocksFeatureClient": BlocksFeatureClient_17,
   "/payload/components/BeforeDashboard#default": default_18,
