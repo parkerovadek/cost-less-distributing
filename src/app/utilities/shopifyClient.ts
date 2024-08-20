e3773cb11ffbd204ddf00b8ecc8f3f4bbe60d756
Parker Ovadek <povadek@bizstream.com>
2024-08-20 11:24:33 -0400

    Updated env variables

--- src/app/utilities/shopifyClient.ts
+++ src/app/utilities/shopifyClient.ts
@@ -3,5 +3,5 @@ import { createStorefrontApiClient } from '@shopify/storefront-api-client';
 export const shopifyClient = createStorefrontApiClient({
   storeDomain: process.env.SHOPIFY_STORE_DOMAIN,
   publicAccessToken: process.env.SHOPIFY_STOREFRONT_API_ACCESS_TOKEN,
-  apiVersion: '2024-10',
+  apiVersion: process.env.SHOPIFY_STOREFRONT_API_VERSION,
 });
